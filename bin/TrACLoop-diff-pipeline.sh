c4cca4b971436fb7e01e689cdc0f0108d2c0d2eb
binbinlai2012 <37155771+binbinlai2012@users.noreply.github.com>
2018-08-07 12:06:08 -0400

    Add files via upload

--- bin/TrACLoop-diff-pipeline.sh
+++ bin/TrACLoop-diff-pipeline.sh
@@ -1,25 +1,32 @@
 #$_1: r150_bed_A $_2: r150_bed_B $_3: bedpe_A $_4: bedpe_B $_5: Loop_A $_6: Loop_B $_7: FDR_thr
 
-echo "${PWD}/merge_tracloop merged_loops.txt merged_anchors.txt $6 $7"
-${PWD}/merge_tracloop merged_loops.txt merged_anchors.txt $6 $7
+workdir=`pwd`
 
-echo "${PWD}/count_tag_on_region merged_anchors.txt $1 Acc_on_merged_anchors_A.txt"
-${PWD}/count_tag_on_region	merged_anchors.txt $1 Acc_on_merged_anchors_A.txt
+cd `dirname $0`
+scriptdir=`pwd`
 
-echo "${PWD}/count_tag_on_region merged_anchors.txt $2 Acc_on_merged_anchors_B.txt"
-${PWD}/count_tag_on_region merged_anchors.txt $2 Acc_on_merged_anchors_B.txt
+cd $workdir
 
-echo "${PWD}/get_petc_in_loop_from_bedpe merged_loops.txt $3 PETc_on_merged_loops_A.txt"
-${PWD}/get_petc_in_loop_from_bedpe merged_loops.txt $3 PETc_on_merged_loops_A.txt
+echo "${scriptdir}/merge_tracloop merged_loops.txt merged_anchors.txt $6 $7"
+${scriptdir}/merge_tracloop merged_loops.txt merged_anchors.txt $6 $7
 
-echo "${PWD}/get_petc_in_loop_from_bedpe merged_loops.txt $4 PETc_on_merged_loops_B.txt"
-${PWD}/get_petc_in_loop_from_bedpe merged_loops.txt $4 PETc_on_merged_loops_B.txt
+echo "${scriptdir}/count_tag_on_region merged_anchors.txt $1 Acc_on_merged_anchors_A.txt"
+${scriptdir}/count_tag_on_region	merged_anchors.txt $1 Acc_on_merged_anchors_A.txt
 
-echo "${PWD}/call_diff_loop PETc_on_merged_loops_A.txt PETc_on_merged_loops_B.txt Acc_on_merged_anchors_A.txt Acc_on_merged_anchors_B.txt merged_loops_comp_summary"
-${PWD}/call_diff_loop PETc_on_merged_loops_A.txt PETc_on_merged_loops_B.txt Acc_on_merged_anchors_A.txt Acc_on_merged_anchors_B.txt merged_loops_comp_summary
+echo "${scriptdir}/count_tag_on_region merged_anchors.txt $2 Acc_on_merged_anchors_B.txt"
+${scriptdir}/count_tag_on_region merged_anchors.txt $2 Acc_on_merged_anchors_B.txt
 
-echo "python ${PWD}/cal_qvalue_diff_tracloop.py merged_loops_comp_summary merged_loops_comp_summary2"
-python ${PWD}/cal_qvalue_diff_tracloop.py merged_loops_comp_summary merged_loops_comp_summary2
+echo "${scriptdir}/get_petc_in_loop_from_bedpe merged_loops.txt $3 PETc_on_merged_loops_A.txt"
+${scriptdir}/get_petc_in_loop_from_bedpe merged_loops.txt $3 PETc_on_merged_loops_A.txt
 
-echo "python ${PWD}/get_significant_diff_tracloop.py merged_loops_comp_summary2 ${FDR_thr} merged_loops_increased_FDR${FDR_thr} merged_loops_decreased_FDR${FDR_thr} merged_loops_nochange_FDR${FDR_thr}"
-python ${PWD}/get_significant_diff_tracloop.py merged_loops_comp_summary2 ${FDR_thr} merged_loops_increased_FDR${FDR_thr} merged_loops_decreased_FDR${FDR_thr} merged_loops_nochange_FDR${FDR_thr} 
+echo "${scriptdir}/get_petc_in_loop_from_bedpe merged_loops.txt $4 PETc_on_merged_loops_B.txt"
+${scriptdir}/get_petc_in_loop_from_bedpe merged_loops.txt $4 PETc_on_merged_loops_B.txt
+
+echo "${scriptdir}/call_diff_loop PETc_on_merged_loops_A.txt PETc_on_merged_loops_B.txt Acc_on_merged_anchors_A.txt Acc_on_merged_anchors_B.txt merged_loops_comp_summary"
+${scriptdir}/call_diff_loop PETc_on_merged_loops_A.txt PETc_on_merged_loops_B.txt Acc_on_merged_anchors_A.txt Acc_on_merged_anchors_B.txt merged_loops_comp_summary
+
+echo "python ${scriptdir}/cal_qvalue_diff_tracloop.py merged_loops_comp_summary merged_loops_comp_summary2"
+python ${scriptdir}/cal_qvalue_diff_tracloop.py merged_loops_comp_summary merged_loops_comp_summary2
+
+echo "python ${scriptdir}/get_significant_diff_tracloop.py merged_loops_comp_summary2 ${FDR_thr} merged_loops_increased_FDR${FDR_thr} merged_loops_decreased_FDR${FDR_thr} merged_loops_nochange_FDR${FDR_thr}"
+python ${scriptdir}/get_significant_diff_tracloop.py merged_loops_comp_summary2 ${FDR_thr} merged_loops_increased_FDR${FDR_thr} merged_loops_decreased_FDR${FDR_thr} merged_loops_nochange_FDR${FDR_thr} 
--- bin/TrACLoop_pipeline.sh
+++ bin/TrACLoop_pipeline.sh
@@ -1,18 +1,25 @@
 #$_1: bedpe  $_2: peak_file(MACS/SICER)  $_3: tag  $_4: chrlen  $_5: win  $_6: minLength  $_7: maxLength  $_8: min_PETs  $_9: FDR_thr
 
+workdir=`pwd`
+
+cd `dirname $0`
+scriptdir=`pwd`
+
+cd $workdir
+
 # call interaction background model
-${PWD}/call_interaction_model -L $4 -b $1 -p $3 -w $5 -l $6 -u $7 -R $2 -d 0.1
+${scriptdir}/call_interaction_model -L $4 -b $1 -p $3 -w $5 -l $6 -u $7 -R $2 -d 0.1
 
-Rscript ${PWD}/smooth_spline.r ${3}.distance.dist.txt 0.25 ${3}.distance.dist.sm.txt
+Rscript ${scriptdir}/smooth_spline.r ${3}.distance.dist.txt 0.25 ${3}.distance.dist.sm.txt
 
-Rscript ${PWD}/smooth_spline.r ${3}.distance.PETs_dist.txt 0.5 ${3}.distance.PETs_dist.sm.txt
+Rscript ${scriptdir}/smooth_spline.r ${3}.distance.PETs_dist.txt 0.5 ${3}.distance.PETs_dist.sm.txt
 
-Rscript ${PWD}/smooth_spline.r ${3}.accm.dist.txt 0.25 ${3}.accm.dist.sm.txt 
+Rscript ${scriptdir}/smooth_spline.r ${3}.accm.dist.txt 0.25 ${3}.accm.dist.sm.txt 
 
-Rscript ${PWD}/smooth_spline.r ${3}.accm.PETs_dist.txt 0.25 ${3}.accm.PETs_dist.sm.txt 
+Rscript ${scriptdir}/smooth_spline.r ${3}.accm.PETs_dist.txt 0.25 ${3}.accm.PETs_dist.sm.txt 
 
 # call interaction
-${PWD}/call_interaction -R $2 -b $1 -p $3 -c 1 -w $5 -l $6 -u $7 \
+${scriptdir}/call_interaction -R $2 -b $1 -p $3 -c 1 -w $5 -l $6 -u $7 \
 	-0 ${3}.distance.dist0.txt \
 	-1 ${3}.distance.dist.sm.txt \
 	-2 ${3}.distance.PETs_dist0.txt \
@@ -24,8 +31,38 @@ ${PWD}/call_interaction -R $2 -b $1 -p $3 -c 1 -w $5 -l $6 -u $7 \
 #	-8 ${3}.total_valid_PETs_in_anchor.txt 
 
 # call FDR
-python ${PWD}/cal_qvalue.py ${3}.interaction.summary ${3}.interaction.summary.fdr
-python ${PWD}/flt_sig_interaction.py ${3}.interaction.summary.fdr ${8} ${9} ${3}.interaction.c${8}.fdr${9}
+python ${scriptdir}/cal_qvalue.py ${3}.interaction.summary ${3}.interaction.summary.fdr
+python ${scriptdir}/flt_sig_interaction.py ${3}.interaction.summary.fdr ${8} ${9} ${3}.interaction.c${8}.fdr${9}
 
 # merge significant interaction
-${PWD}/merge_sig_interaction ${3}.interaction.c${8}.fdr${9} $1 $6 $7 ${3}.total_valid_PETs.txt $3
+${scriptdir}/merge_sig_interaction ${3}.interaction.c${8}.fdr${9} $1 $6 $7 ${3}.total_valid_PETs.txt $3
+
+# delete temporary files
+rm -f comp_distance_PETs_p.txt
+rm -f comp_distance_p.txt
+rm -f comp_rpm_PETs_p.txt 
+rm -f comp_rpm_p.txt 
+rm -f ${3}.accm.PETs_dist.sm.txt 
+rm -f ${3}.accm.PETs_dist.txt
+rm -f ${3}.accm.dist.sm.txt
+rm -f ${3}.accm.dist.txt
+rm -f ${3}.distance.PETs_dist.sm.txt
+rm -f ${3}.distance.PETs_dist.txt
+rm -f ${3}.distance.PETs_dist0.txt
+rm -f ${3}.distance.dist.sm.txt 
+rm -f ${3}.distance.dist.txt
+rm -f ${3}.distance.dist0.txt
+rm -f ${3}.interaction.c3.fdr0.05
+rm -f ${3}.interaction.summary
+rm -f ${3}.rpm_dist.txt
+rm -f ${3}.total_valid_PETs.txt
+rm -f ${3}.total_valid_PETs_in_anchor.txt
+rm -f ${3}.total_valid_space_size.txt
+rm -f tmp_pets.txt
+
+
+
+
+ 
+
+
